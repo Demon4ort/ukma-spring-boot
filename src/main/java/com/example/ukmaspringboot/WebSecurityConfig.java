f386a57c6de665baba0f6ea9a8d94011ecf4653a
romashaa <mariia.romanenko@ukma.edu.ua>
2021-12-09 11:37:57 +0200

    course deleted

--- src/main/java/com/example/ukmaspringboot/WebSecurityConfig.java
+++ src/main/java/com/example/ukmaspringboot/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+        http.cors().and().csrf().disable().formLogin()
                 .loginPage("/login")
                 .usernameParameter("email")
                 .defaultSuccessUrl("/home")
